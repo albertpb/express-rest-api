dfa75f45a75fe2919c967c4c046bb8cb06b403a9
Unknown <palumbo.alb@gmail.com>
2018-07-23 23:27:58 -0400

    Update libraries

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,18 +1,33 @@
 import chalk from 'chalk';
-import log from 'winston';
+import winston from 'winston';
 import path from 'path';
 
 // set log as cli mode
-log.cli();
+const logger = winston.createLogger({
+	level: 'info',
+	format: winston.format.simple(),
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+	logger.add(
+		new winston.transports.Console({
+			format: winston.format.simple(),
+		}),
+	);
+}
 
 function logServerConfig() {
 	const url = ['http://', process.env.HOST, ':', process.env.PORT].join('');
 
-	log.info(chalk.red('=========================================='));
-	log.info(chalk.blue('Environment:'), process.env.NODE_ENV);
-	log.info(chalk.blue('Listening at:'), url);
-	log.info(chalk.blue('Directory:'), path.resolve('src'));
-	log.info(chalk.red('=========================================='));
+	logger.info(chalk.red('=========================================='));
+	logger.info(chalk.blue('Environment:') + ' ' + process.env.NODE_ENV);
+	logger.info(chalk.blue('Listening at:') + ' ' + url);
+	logger.info(chalk.blue('Directory:') + ' ' + path.resolve('src'));
+	logger.info(chalk.red('=========================================='));
 }
 
 function colorfulLog(tokens, req, res) {
